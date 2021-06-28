f089f76a04c7e71c751991f2ce985fe4550ef115
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-06-28 13:30:48 +0700

    header

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 function Header() {
     return (
@@ -9,9 +20,46 @@ function Header() {
                     src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png"
                     alt=""
                 />
+                <div className="header__input">
+                    <SearchIcon/>
+                    <input type='text'/>
+                </div>
             </div>
-            <div className="header__midle"></div>
-            <div className="header__right"></div>
+            <div className="header__center">
+                <div className="header__option">
+                    <HomeIcon/>
+                </div>
+                <div className="header__option">
+                    <FlagIcon/>
+                </div>
+                <div className="header__option">
+                    <SubscriptionsIcon/>
+                </div>
+                <div className="header__option">
+                    <StorefrontIcon/>
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon/>
+                </div>
+            </div>
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar/>
+                    <h4>Marshall</h4>
+                </div>
+                <IconButton>
+                    <AddRoundedIcon/>
+                </IconButton>
+                <IconButton>
+                    <ForumIcon/>
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon/>
+                </IconButton>
+                <IconButton>
+                    <ArrowDropDownIcon/>
+                </IconButton>
+            </div>
         </div>
     )
 }
