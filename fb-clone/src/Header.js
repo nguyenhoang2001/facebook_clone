6ff262abbb4c999817742fc313fea88aff91a25f
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-06-28 11:08:00 +0700

    include material ui

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -4,7 +4,14 @@ import './Header.css'
 function Header() {
     return (
         <div className='header'>
-            div.header
+            <div className="header__left">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png"
+                    alt=""
+                />
+            </div>
+            <div className="header__midle"></div>
+            <div className="header__right"></div>
         </div>
     )
 }
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB71MEizusuHkc-WXYY4bGsqBnDbRPz9Lk",
+    authDomain: "facebook-clone-cd47b.firebaseapp.com",
+    projectId: "facebook-clone-cd47b",
+    storageBucket: "facebook-clone-cd47b.appspot.com",
+    messagingSenderId: "226559844808",
+    appId: "1:226559844808:web:570add7bcaee72da50a0fd"
+  };
