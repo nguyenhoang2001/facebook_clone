6b622650ad234968a0664919442cd6faed7a7224
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-07-06 22:02:03 +0700

    firebase and reduce layout

--- fb-clone/src/Login.js
+++ fb-clone/src/Login.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Login.css'
 import { Button } from '@material-ui/core'
+import { auth, provider } from './firebase'
 
 function Login() {
     const signIn = () => {
+        auth.signInWithPopup(provider).then((result) => {
+            console.log(result.user)
+        }).catch((error) => alert(error.message));
+    };
 
-    }
     return (
         <div className='login'>
             <div className="login__logo">
---
+++ fb-clone/src/StateProvider.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+
+export const StaeContext = createContext();
+
+export const StateProvider = ({reducer, initialState, childreen}) => (
+    <StateContext.Provider value= {useReducer(reducer,initialState)}>
+        {childreen}
+    </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StaeContext);
+
